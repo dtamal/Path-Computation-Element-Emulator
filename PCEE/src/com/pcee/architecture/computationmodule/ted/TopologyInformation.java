9abed84074562b10c5e43988912e43a480f0b00f
Mohit <mohitc@gmail.com>
2012-12-13 16:20:12 +0100

    Added a process which listens for topology updates received in the form of JSON strings on the topologyUpdatePort

--- PCEE/src/com/pcee/architecture/computationmodule/ted/TopologyInformation.java
+++ PCEE/src/com/pcee/architecture/computationmodule/ted/TopologyInformation.java
@@ -17,8 +17,20 @@
 
 package com.pcee.architecture.computationmodule.ted;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Map;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.graph.elements.edge.EdgeElement;
+import com.graph.elements.edge.params.EdgeParams;
+import com.graph.elements.edge.params.impl.BasicEdgeParams;
 import com.graph.graphcontroller.Gcontroller;
 import com.graph.graphcontroller.impl.GcontrollerImpl;
 import com.graph.topology.importers.ImportTopology;
@@ -34,6 +46,16 @@ import com.pcee.logger.Logger;
  */
 public class TopologyInformation {
 
+
+	//JSON Parser
+	private static Gson json = new Gson();
+
+	//Port on which to listen for topology Information Updates
+	private static int topologyUpdatePort = 5555;
+
+	//Thread for topology Update Listener
+	private static Thread topologyUpdateThread;
+
 	// Static oject instance of the TopologyInformation Class
 	static private TopologyInformation _instance;
 
@@ -45,6 +67,15 @@ public class TopologyInformation {
 
 	// path to the topology description file
 	private static String topoPath = ".//atlanta.txt";
+
+
+	/**Function to set the port for topology Updates
+	 *
+	 * @param port
+	 */
+	public static void setTopologyUpdatePort(int port){
+		topologyUpdatePort = port;
+	}
 
 	/**
 	 * @param input
@@ -83,6 +114,10 @@ public class TopologyInformation {
 			localDebugger("Error in loading graph from file");
 		else
 			localLogger("NetworkSize: " + networkSize());
+
+		//Start Topology Update Listener
+		localLogger ("Starting thread to listen for topology updates on port " + topologyUpdatePort);
+		startTopologyUpdateListner();
 	}
 
 	/**
@@ -146,5 +181,157 @@ public class TopologyInformation {
 	 */
 	public static void main(String[] args) {
 		System.out.println(new File(topoPath).getAbsolutePath());
+	}
+
+
+
+	/** Function to initialize a thread to listen for topology updates */
+	private void startTopologyUpdateListner() {
+		topologyUpdateThread = new Thread() {
+
+			//Function to parse and implement incoming topology Updates
+			@SuppressWarnings("rawtypes")
+			public void parseInput(String text) {
+				try {
+					Map input = json.fromJson(text, Map.class);
+					if (input.containsKey("operation")) {
+						if (input.get("operation").toString().equalsIgnoreCase("reserve")) {
+							//Request to reserve capacity on a sequence of nodes
+							double capacity = Double.parseDouble(input.get("capacity").toString());
+							ArrayList vertexSequence = ((ArrayList)input.get("vertexSequence"));
+							synchronized(graph) {
+								for (int i=0;i<vertexSequence.size()-1;i++){
+									String sourceID = (String)vertexSequence.get(i);
+									String destID = (String)vertexSequence.get(i+1);
+									if (graph.aConnectingEdge(sourceID, destID)) {
+										if (graph.getConnectingEdge(sourceID, destID).getEdgeParams().reserveCapacity(capacity)) {
+											localLogger("Cannot reserve requested capacity between " + sourceID +" and " + destID);
+											for (int j=0;j<i;j++) {
+												//Releasing capacity that was reserved till before i
+												String srcID = (String)vertexSequence.get(j);
+												String dstID = (String)vertexSequence.get(j+1);
+												graph.getConnectingEdge(srcID, dstID).getEdgeParams().releaseCapacity(capacity);
+											}
+											break;
+
+										}
+									} else {
+										localLogger("Invalid Vertex Sequence sent, no edge found between " + sourceID +" and " + destID);
+										for (int j=0;j<i;j++) {
+											//Releasing capacity that was reserved till before i
+											String srcID = (String)vertexSequence.get(j);
+											String dstID = (String)vertexSequence.get(j+1);
+											graph.getConnectingEdge(srcID, dstID).getEdgeParams().releaseCapacity(capacity);
+										}
+										break;
+									}
+								}
+							}
+
+						} else if (input.get("operation").toString().equalsIgnoreCase("release")) {
+							//Request to reserve capacity on a sequence of nodes
+							double capacity = Double.parseDouble(input.get("capacity").toString());
+							ArrayList vertexSequence = ((ArrayList)input.get("vertexSequence"));
+							synchronized(graph) {
+								for (int i=0;i<vertexSequence.size()-1;i++){
+									String sourceID = (String)vertexSequence.get(i);
+									String destID = (String)vertexSequence.get(i+1);
+									if (graph.aConnectingEdge(sourceID, destID)) {
+										if (!graph.getConnectingEdge(sourceID, destID).getEdgeParams().releaseCapacity(capacity)) {
+											localLogger("Cannot release additional capacity between " + sourceID +" and " + destID);
+											for (int j=0;j<i;j++) {
+												//Releasing capacity that was reserved till before i
+												String srcID = (String)vertexSequence.get(j);
+												String dstID = (String)vertexSequence.get(j+1);
+												graph.getConnectingEdge(srcID, dstID).getEdgeParams().reserveCapacity(capacity);
+											}
+											break;
+
+										}
+									} else {
+										localLogger("Invalid Vertex Sequence sent, no edge found between " + sourceID +" and " + destID);
+										for (int j=0;j<i;j++) {
+											//Releasing capacity that was reserved till before i
+											String srcID = (String)vertexSequence.get(j);
+											String dstID = (String)vertexSequence.get(j+1);
+											graph.getConnectingEdge(srcID, dstID).getEdgeParams().reserveCapacity(capacity);
+										}
+										break;
+									}
+								}
+							}
+
+						} else 	if (input.get("operation").toString().equalsIgnoreCase("updateEdgeDefinition")) {
+							//Request to update the Edge Definition
+							double capacity = Double.parseDouble(input.get("capacity").toString());
+							double avcapacity = Double.parseDouble(input.get("avcapacity").toString());
+							double weight = Double.parseDouble(input.get("weight").toString());
+							double delay = Double.parseDouble(input.get("delay").toString());
+							ArrayList vSequence = ((ArrayList)input.get("vertexSequence"));
+							synchronized(graph) {
+								ArrayList<String> vertexSequence = new ArrayList<String>();
+								for (int i=0;i<vSequence.size();i++) {
+									vertexSequence.add(vSequence.get(i).toString());
+								}
+								String sourceID = vertexSequence.get(0);
+								String destID = vertexSequence.get(vertexSequence.size()-1);
+								if (graph.aConnectingEdge(sourceID, destID)) {
+									EdgeElement edge = graph.getConnectingEdge(sourceID, destID);
+									EdgeParams params = new BasicEdgeParams(edge, delay, weight, capacity);
+									params.setAvailableCapacity(avcapacity);
+									edge.setEdgeParams(params);
+									localLogger("Updated Edge from " + sourceID + " to " + destID);
+								}
+
+
+							}
+
+						}
+					}
+				} catch (JsonSyntaxException e) {
+					localLogger("Malformed Json sent from Client" + e.getMessage());
+				} catch (Exception e) {
+					localLogger("Invalid parameters sent in the JSON from Client : " + e.getMessage());
+
+				}
+
+			}
+
+
+			// Override the run() method to implement a simple server socket to listen for topology updates
+			public void run() {
+				ServerSocket serverSocket;
+				try {
+					serverSocket = new ServerSocket(topologyUpdatePort);
+
+					while (true) {
+						try {
+							Socket clientSocket = serverSocket.accept();
+							// Remote connection sends topology in the form of a string with a delimiter "@" used for each line
+							BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+							String text = "";
+							String line = "";
+							while ((line = bufferedReader.readLine()) != null) {
+								text = text + line;
+							}
+							parseInput(text);
+							// Close the socket
+							bufferedReader.close();
+							clientSocket.close();
+						} catch (IOException e) {
+							localDebugger("IOException during read for new connections. Discarding update");
+							continue;
+						} 
+					}
+				} catch (IOException e1) {
+					localDebugger("Could not open server socket to listen for topology updates on port:" + topologyUpdatePort);
+
+				}
+
+			}
+
+		};
+		topologyUpdateThread.setName("TopologyUpdateThread");
+		topologyUpdateThread.start();
 	}
 }
